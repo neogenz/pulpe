56fab30cafb9b07447289f202c2162ea7d2ddcbb
Maxime De Sogus <maxime.desogus@gmail.com>
2025-12-22 20:59:39 +0100

    build: configure zoneless Angular and format

--- frontend/projects/webapp/src/main.ts
+++ frontend/projects/webapp/src/main.ts
@@ -1,14 +1,11 @@
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from '@app/app.config';
 import { App } from '@app/app';
-import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
+import { appConfig } from '@app/app.config';
 import { environment } from '@env/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(App, {
-  ...appConfig,
-  providers: [provideZoneChangeDetection(), ...appConfig.providers],
-}).catch((err) => console.error(err));
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
