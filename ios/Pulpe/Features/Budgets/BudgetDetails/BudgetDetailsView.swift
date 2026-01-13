de0aecb7bf6593591dc6cff5fa6ecd45bcfcc4e5
Maxime De Sogus <maxime.desogus@gmail.com>
2026-01-13 14:07:07 +0100

    fix(ios): add compiler check for iOS 26 glassEffect API

--- ios/Pulpe/Features/Budgets/BudgetDetails/BudgetDetailsView.swift
+++ ios/Pulpe/Features/Budgets/BudgetDetails/BudgetDetailsView.swift
@@ -45,11 +45,15 @@ struct MonthNavigationBar: View {
 
 private struct GlassBackgroundModifier: ViewModifier {
     func body(content: Content) -> some View {
+        #if compiler(>=6.2)
         if #available(iOS 26.0, *) {
             content.glassEffect(in: .capsule)
         } else {
             content.background(.ultraThinMaterial, in: Capsule())
         }
+        #else
+        content.background(.ultraThinMaterial, in: Capsule())
+        #endif
     }
 }
 
