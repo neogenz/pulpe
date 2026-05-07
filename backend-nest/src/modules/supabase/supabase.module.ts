952221fd9440311ddad0afb9e979ecc11fce25ed
Maxime De Sogus <maxime.desogus@gmail.com>
2026-05-08 00:37:49 +0200

    refactor(backend): introduce AuthenticatedSupabaseProvider

--- backend-nest/src/modules/supabase/supabase.module.ts
+++ backend-nest/src/modules/supabase/supabase.module.ts
@@ -1,9 +1,10 @@
 import { Global, Module } from '@nestjs/common';
 import { SupabaseService } from './supabase.service';
+import { AuthenticatedSupabaseProvider } from './authenticated-supabase.provider';
 
 @Global()
 @Module({
-  providers: [SupabaseService],
-  exports: [SupabaseService],
+  providers: [SupabaseService, AuthenticatedSupabaseProvider],
+  exports: [SupabaseService, AuthenticatedSupabaseProvider],
 })
 export class SupabaseModule {}
