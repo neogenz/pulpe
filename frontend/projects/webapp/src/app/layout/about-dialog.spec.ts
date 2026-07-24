12227256327e2dea4f791d818f2eb7791335813d
Maxime De Sogus <maxime.desogus@gmail.com>
2026-07-24 08:50:02 +0200

    test(frontend): use redirectTo stub for AboutDialog catch-all route

--- frontend/projects/webapp/src/app/layout/about-dialog.spec.ts
+++ frontend/projects/webapp/src/app/layout/about-dialog.spec.ts
@@ -26,7 +26,7 @@ describe('AboutDialog', () => {
       imports: [AboutDialog],
       providers: [
         provideZonelessChangeDetection(),
-        provideRouter([{ path: '**', children: [] }]),
+        provideRouter([{ path: '**', redirectTo: '' }]),
         ...provideTranslocoForTest(),
         { provide: MatDialogRef, useValue: mockDialogRef },
         { provide: ApplicationConfiguration, useValue: mockApplicationConfig },
