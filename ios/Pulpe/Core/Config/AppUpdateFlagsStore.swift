8ce25bc48e25b09ac913d50b9f01c7412e64db88
Maxime De Sogus <maxime.desogus@gmail.com>
2026-08-16 17:35:57 +0200

    refactor(ios): move update flags into core

--- ios/Pulpe/Core/Config/AppUpdateFlagsStore.swift
+++ ios/Pulpe/Core/Config/AppUpdateFlagsStore.swift
@@ -22,7 +22,6 @@ struct AppUpdateFlagsStore: AppUpdateFlagsStoring, @unchecked Sendable {
     }
 
     func setLastPromptedVersion(_ version: String) {
-        guard lastPromptedVersion.map({ $0.isSemVerBelow(version) }) ?? true else { return }
         defaults.set(version, forKey: Key.lastPromptedVersion)
     }
 }
